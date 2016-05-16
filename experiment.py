cd6068c1aa34bfd2f6ccb74f3f8b406c326f64d7
J. Mulle <this.impetus@gmail.com>
2016-05-16 10:39:02 -0300

    Figure drawing now exists in a separate class. Parameterization of path much improved.

--- experiment.py
+++ experiment.py
@@ -25,36 +25,65 @@ BOT_L = 0
 TOP_L = 1
 TOP_R = 2
 
+"""
+ Everything involved in the bezier function is patched together from various stack overflow investigations. The linear
+ one is my own.
 
+ I'm using these to create an animation of a dot following through a path of connected linear and bezier segments; the
+ full script creates a big list of segments where each end point is the beginning of the next segment's start point.
+ The problem, isn't that this don't work; it does, and quite well, really. The problem is that I have:
+
+ a) made no effort to control the resolution of the output, so I have a very different delta d, per point, between large
+ curves vs small curves, and either vs lines. The amounts to an extremely variously paced animation if each point is
+ treated as a frame.
+
+ b) don't really know *how* to control said resolution—especially in the bezier condition, where the path is closer to
+ magic to me than something I really understand (I mean, roughly and abstractly I actually do understand it, but not in
+ a practically useful way).
+
+ One solution I tried to to use was to create an algorithm for "dropping frames", or rather only including frames at
+ every n distance; this improved things a lot, but not enough, because the apparent speed around a curve is still much
+ slower than over a line, even if those curves are nothing but little lines themselves. I included that solution here.
+
+ What I think would be the ideal case is to reverse-engineer these functions—to make them output points that correspond
+ to a certain frame rate. But any solution would really work for me.
+
+ If you wanted to see this in action you could clone https://github.com/jmwmulle/TraceLab, but you'd need the newest
+ version of klibs which will require you to brew a few dependencies first and possibly install some business with pip.
+ it might be more useful to do so, becuse the animate() method of the DrawFigure class (of which the included def is
+ a modification) will also overlay the path the dot is traversing, which I, at least, found much more informative.
+ It will also generate the path itself, which is useful because the problems I've described don't rally surface until
+ you're joining a variety of segments together.
+"""
 def pascal_row(n):
-    # This returns the nth row of Pascal's Triangle
-    result = [1]
-    x, numerator = 1, n
-    for denominator in range(1, n//2+1):
-        # print(numerator,denominator,x)
-        x *= numerator
-        x /= denominator
-        result.append(x)
-        numerator -= 1
-    if n&1 == 0:
-        # n is even
-        result.extend(reversed(result[:-1]))
-    else:
-        result.extend(reversed(result))
-    return result
+	# This returns the nth row of Pascal's Triangle
+	result = [1]
+	x, numerator = 1, n
+	for denominator in range(1, n//2+1):
+		# print(numerator,denominator,x)
+		x *= numerator
+		x /= denominator
+		result.append(x)
+		numerator -= 1
+					  # n is even							  n is odd
+		result.extend(reversed(result[:-1]) if n & 1 == 0 else reversed(result))
+	return result
 
 
-def bezier_interpolation(origin, destination, control_o, control_d=None, segmented=True, integers=True):
+
+def bezier_interpolation(origin, destination, control_o, control_d=None):
 		destination = tuple(destination)
 		origin = tuple(origin)
-		control_o = tuple(origin)
+		control_o = tuple(control_o)
 		if control_d:
 			control_d = tuple(control_d)
 		points = [origin, control_o, control_d, destination] if control_d else [origin, control_o, destination]
+		break_next = False
+		segments = []
 		n = len(points)
-		combinations = pascal_row(n - 1)
 
 		def bezier(transitions):
+			combinations = pascal_row(n - 1)
 			result = []
 			for t in transitions:
 				t_powers = (t ** i for i in range(n))
@@ -63,36 +92,8 @@ def bezier_interpolation(origin, destination, control_o, control_d=None, segment
 				result.append(list(sum([coef * p for coef, p in zip(coefficients, ps)]) for ps in zip(*points)))
 			return result
 
-		break_next = False
-		segments = []
-		points = []
-		print bezier([0.01 * t for t in range(101)])
-		for pos in chunk(bezier([0.01 * t for t in range(101)]), 2):
-			try:
-				if integers:
-					pos[0] = tuple(int(i) for i in pos[0])
-					pos[1] = tuple(int(i) for i in pos[1])
-				if break_next:
-					segments.append([break_next, destination])
-					points.append(break_next)
-					points.append(destination)
-					break
-				if pos[0] == destination:
-					break_next = pos[0]
-					continue
-				if len(pos[0]) == 2 and len(pos[1]) == 2:
-					segments.append(pos)
-					points.append(pos[0])
-					points.append(pos[1])
-			except IndexError:
-				break
-		if segmented:
-			return segments if len(segments[-1]) == 2 else segments[:-1]
-		else:
-			return points
+		return [ (int(p[0]), int(p[1])) for p in bezier([0.01 * t for t in range(101)])]
 
-print bezier_interpolation((0,0), (100,00), (50,0), segmented=False)
-exit()
 
 def linear_interpolation(origin, destination, segmented=True):
 	d_x = destination[0] - origin[0]
@@ -110,6 +111,69 @@ def linear_interpolation(origin, destination, segmented=True):
 		return points
 
 
+def path_len(frames):
+	# where frames is a list of coordinate tuples
+	path_len = 0
+	for i in range(0, len(frames)):
+		try:
+			p1 = [1.0 * frames[i][0], 1.0 * frames[i][1]]
+			p2 = [1.0 * frames[i + 1][0], 1.0 * frames[i + 1][1]]
+			path_len += line_segment_len(p1, p2)
+		except IndexError:
+			p1 = [1.0 * frames[i][0], 1.0 * frames[i][1]]
+			p2 = [1.0 * frames[0][0], 1.0 * frames[0][1]]
+			path_len += line_segment_len(p1, p2)
+	return path_len
+
+
+def animate(self, duration):
+		surface = self.render()
+		draw_in = float(duration)
+		rate = 0.01666666667
+		max_frames = int(draw_in / rate)
+		delta_d = self.path_len / max_frames
+		print draw_in, self.path_len, delta_d
+		a_frames = [self.frames[0]]
+		seg_len = 0
+		for i in range(0, len(self.frames)):
+			try:
+				p1 = [1.0 * self.frames[i][0], 1.0 * self.frames[i][1]]
+				p2 = [1.0 * self.frames[i + 1][0], 1.0 * self.frames[i + 1][1]]
+				seg_len += line_segment_len(p1, p2)
+			except IndexError:
+				p1 = [1.0 * self.frames[i][0], 1.0 * self.frames[i][1]]
+				p2 = [1.0 * self.frames[0][0], 1.0 * self.frames[0][1]]
+				seg_len += line_segment_len(p1, p2)
+			if seg_len >= delta_d:
+				a_frames.append(self.frames[i])
+				seg_len = 0
+
+		# skip_frame = int(len(frames) / max_frames)
+		# removed = 0
+		# while len(frames) > max_frames:
+		# 	removed += 1
+		# 	try:
+		# 		frames.remove(frames[removed * skip_frame])
+		# 	except IndexError:
+		# 		break
+		# trunc_frames = [item for index, item in enumerate(frames) if (index + 1) % skip_frame != 0]
+
+		last_f = time.time()
+		f = 0
+		frame_start = time.time()
+		for f in a_frames:
+			while time.time() < rate + last_f:
+				pump()
+			# f += 1
+			# pos = a_frames[f]
+			self.exp.fill()
+			self.exp.blit(surface, 5, Params.screen_c)
+			self.exp.blit(self.exp.tracker_dot, 5, f)
+			self.exp.flip()
+			last_f = time.time()
+		print time.time() - frame_start
+
+
 class TraceLab(klibs.Experiment):
 	# graphical elements
 	canvas = None
@@ -125,7 +189,7 @@ class TraceLab(klibs.Experiment):
 	origin_pos = None
 	origin_boundary = None
 	tracker_dot_proto = None
-	tracker_dot_size = 5
+	tracker_dot_size = 3
 	tracker_dot = None
 
 	instructions_1 = None
@@ -150,15 +214,37 @@ class TraceLab(klibs.Experiment):
 
 
 	def setup(self):
-		self.text_manager.add_style('instructions', 32, [255, 255, 255, 255])
-		self.text_manager.add_style('tiny', 12, [255, 255,255, 255])
-		self.figure = DrawFigure(self)
-		self.quit()
-		self.canvas = Rectangle(self.canvas_size, fill=self.canvas_color, stroke=[2, self.canvas_border, STROKE_INNER])
+		# c = bezier_interpolation( (10,10), (100,100), (10, 500), segmented=False)
+		# print c
+		# surf = aggdraw.Draw("RGBA", (800, 800), (255, 255, 255, 255))
+		# p_str = "M10 10"
+		# for i in range(0, len(c)):
+		# 	try:
+		# 		p_str += " L{0} {1} {2} {3}".format(*(list(c[i]) + list(c[i+1])))
+		# 	except IndexError:
+		# 		pass
+		# sym = aggdraw.Symbol(p_str)
+		# surf.symbol((0, 0), sym, aggdraw.Pen((255, 0, 0), 1, 255))
+		# surf = aggdraw_to_array(surf)
+		# self.fill()
+		# # for p in c:
+		# # 	self.blit(self.tracker_dot)
+		# self.blit(surf)
+		# self.flip()
+		#
+		# self.any_key()
+		# self.quit()
+		#
+		# self.canvas = Rectangle(self.canvas_size, fill=self.canvas_color, stroke=[2, self.canvas_border, STROKE_INNER])
 		self.origin_proto = Ellipse(self.origin_size)
 		self.tracker_dot_proto = Ellipse(self.tracker_dot_size)
 		self.tracker_dot_proto.fill = [255, 0, 0]
 		self.tracker_dot = self.tracker_dot_proto.render()
+		self.text_manager.add_style('instructions', 32, [255, 255, 255, 255])
+		self.text_manager.add_style('tiny', 12, [255, 255,255, 255])
+		self.figure = DrawFigure(self)
+		self.figure.animate(10)
+		self.quit()
 		self.origin_proto.fill = self.origin_active_color
 		self.origin_active = self.origin_proto.render()
 		self.origin_proto.fill = self.origin_inactive_color
@@ -307,14 +393,13 @@ class DrawFigure(object):
 		self.total_spf = 0
 		self.points = []
 		self.segments = []
-		self.segment_compilation = None
+		self.frames = None
+		self.path_len = 0
 		self.__generate_null_points__()
 		self.__gen_quad_intersects__()
 		self.__gen_real_points__()
 		self.__gen_segments__()
-		for s in self.segments:
-			print s
-		self.draw()
+
 
 	def __generate_null_points__(self):
 		# make sure minimums won't exceed randomly generated total
@@ -339,19 +424,20 @@ class DrawFigure(object):
 	def __gen_quad_intersects__(self):
 		# replaces the first index of each quadrant in self.points with a coordinate tuple
 		i_m = Params.inner_margin
-		o_m = Params.outer_margin
+		o_mh = Params.outer_margin_h
+		o_mv = Params.outer_margin_v
 		s_c = Params.screen_c
 		s_x = Params.screen_x
 		s_y = Params.screen_y
 		self.quad_ranges = [
-			[(0, s_c[1]), (s_c[0], s_y)],
-			[(0,0), s_c],
-			[(s_c[0], 0), (s_x, s_c[1])],
-			[s_c, Params.screen_x_y]]
-		self.points[0][0] = (s_c[0], randrange(s_c[1] + i_m, s_y - o_m))
-		self.points[1][0] = (randrange(o_m, s_c[0] - i_m), s_c[1])
-		self.points[2][0] = (s_c[0], randrange(o_m, s_c[1] - i_m))
-		self.points[3][0] = (randrange(s_c[0] + i_m, s_x - o_m), s_c[1])
+			[(o_mh, s_c[1] + i_m), (s_c[0] - i_m, s_y - o_mv)],
+			[(o_mh, o_mv), (s_c[0] - i_m, s_c[1] - i_m)],
+			[(s_c[0] + i_m, o_mv), (s_x - o_mh, s_c[1] - i_m)],
+			[(s_c[0] + i_m, s_c[1] + i_m), (s_x - o_mh, s_y - o_mv)]]
+		self.points[0][0] = (s_c[0], randrange(s_c[1] + i_m, s_y - o_mv))
+		self.points[1][0] = (randrange(o_mh, s_c[0] - i_m), s_c[1])
+		self.points[2][0] = (s_c[0], randrange(o_mv, s_c[1] - i_m))
+		self.points[3][0] = (randrange(s_c[0] + i_m, s_x - o_mh), s_c[1])
 
 
 	def __gen_real_points__(self):
@@ -366,7 +452,7 @@ class DrawFigure(object):
 		for i in range(0, len(self.points)):
 			curves = int((1.0 - Params.angularity) * 10) * [False]
 			lines = int(Params.angularity * 10) * [True]
-			if choice(curves+lines) and False:
+			if choice(curves+lines):
 				try:
 					self.segments.append(linear_interpolation(self.points[i], self.points[i + 1], segmented=False))
 				except IndexError:
@@ -374,147 +460,92 @@ class DrawFigure(object):
 			else:
 				try:
 					amp = line_segment_len(self.points[i], self.points[i + 1])
-					c = point_pos(midpoint(self.points[i], self.points[i + 1]), amp, 90)
-					self.segments.append(bezier_interpolation(self.points[i], self.points[i + 1], c, segmented=False))
+					r = angle_between(self.points[i], self.points[i + 1])
+					c = point_pos(midpoint(self.points[i], self.points[i + 1]), amp, 90, r)
+					self.segments.append(bezier_interpolation(self.points[i], self.points[i + 1], c))
 				except IndexError:
 					amp = line_segment_len(self.points[i], self.points[0])
 					c = point_pos(midpoint(self.points[i], self.points[0]), amp, 90)
-					self.segments.append(bezier_interpolation(self.points[i], self.points[0], c, segmented=False))
+					self.segments.append(bezier_interpolation(self.points[i], self.points[0], c))
+		self.frames = list(chain(*self.segments))
+		for i in range(0, len(self.frames)):
+			try:
+				p1 = [1.0 * self.frames[i][0], 1.0 * self.frames[i][1]]
+				p2 = [1.0 * self.frames[i + 1][0], 1.0 * self.frames[i + 1][1]]
+				self.path_len += line_segment_len(p1, p2)
+			except IndexError:
+				p1 = [1.0 * self.frames[i][0], 1.0 * self.frames[i][1]]
+				p2 = [1.0 * self.frames[0][0], 1.0 * self.frames[0][1]]
+				self.path_len += line_segment_len(p1, p2)
 
-		print self.segments
-		self.segment_compilation = list(chain(*self.segments))
-		print self.segment_compilation
 
 	def render(self):
 		surf = aggdraw.Draw("RGBA", Params.screen_x_y, Params.default_fill_color)
-		p_str = "M{0} {1}".format(*self.segment_compilation[0])
-		for s in chunk(self.segment_compilation, 2):
+		p_str = "M{0} {1}".format(*self.frames[0])
+		for s in chunk(self.frames, 2):
 			try:
 				p_str += " L{0} {1} {2} {3}".format(*(list(s[0]) + list(s[1])))
 			except IndexError:
 				pass
 		sym = aggdraw.Symbol(p_str)
-		surf.symbol((0, 0), sym, aggdraw.Pen((255, 0, 0), 1, 255))
+		surf.symbol((0, 0), sym, aggdraw.Pen((35, 35, 35), 0.5, 255))
 		return aggdraw_to_array(surf)
 
-	def draw(self):
+	def draw(self, dots=True, flip=True):
 		self.exp.fill()
 		self.exp.blit(self.render())
-		for p in self.points:
-			self.exp.blit(self.r_dot, 5, p)
-			self.exp.message(str(p[0]), "tiny", location=p)
-		self.exp.flip()
-		self.exp.any_key()
+		if dots:
+			for p in self.points:
+				self.exp.blit(self.r_dot, 5, p)
+				self.exp.message(str(p[0]), "tiny", location=p)
+		if flip:
+			self.exp.flip()
 
 
+	def animate(self, duration):
 
+		surface = self.render()
+		draw_in = float(duration)
+		rate = 0.01666666667
+		max_frames = int(draw_in / rate)
+		delta_d = self.path_len / max_frames
+		print draw_in, self.path_len, delta_d
+		a_frames = [self.frames[0]]
+		seg_len = 0
+		for i in range(0, len(self.frames)):
+			try:
+				p1 = [1.0 * self.frames[i][0], 1.0 * self.frames[i][1]]
+				p2 = [1.0 * self.frames[i + 1][0], 1.0 * self.frames[i + 1][1]]
+				seg_len += line_segment_len(p1, p2)
+			except IndexError:
+				p1 = [1.0 * self.frames[i][0], 1.0 * self.frames[i][1]]
+				p2 = [1.0 * self.frames[0][0], 1.0 * self.frames[0][1]]
+				seg_len += line_segment_len(p1, p2)
+			if seg_len >= delta_d:
+				a_frames.append(self.frames[i])
+				seg_len = 0
 
+		# skip_frame = int(len(frames) / max_frames)
+		# removed = 0
+		# while len(frames) > max_frames:
+		# 	removed += 1
+		# 	try:
+		# 		frames.remove(frames[removed * skip_frame])
+		# 	except IndexError:
+		# 		break
+		# trunc_frames = [item for index, item in enumerate(frames) if (index + 1) % skip_frame != 0]
 
-	# 1. generate segments based on quadrant density
-	# 2. distribute path length amongst segments
-	# 3. decide nature of segment (curve/line)
-	# 4. determine curve characteristics
-	# 5. interpolate
-
-	# def generate_segment_positions(self, seg_count, x_offset, y_offset, avg_dist, dist_variance):
-	# 	min_pt_dist = avg_dist - dist_variance
-	# 	max_pt_dist = avg_dist + dist_variance
-	# 	q_pts_x = []
-	# 	q_pts_y = []
-	#
-	# 	while not len(q_pts_x) == seg_count:
-	# 		x = from_range(x_offset, x_offset + 400)
-	# 		try:
-	# 			if not (x in range(q_pts_x[-1] - min_pt_dist, q_pts_x[-1] + min_pt_dist)):
-	# 				q_pts_x.append(x)
-	# 		except IndexError:
-	# 			q_pts_x.append(x)
-	# 	while not len(q_pts_y) == seg_count:
-	# 		y = from_range(y_offset, y_offset + 400)
-	# 		try:
-	# 			if not (y in range(q_pts_y[-1] - min_pt_dist, q_pts_y[-1] + min_pt_dist)):
-	# 				q_pts_y.append(y)
-	# 		except IndexError:
-	# 			q_pts_y.append(y)
-	# 	q_points = []
-	# 	for p in q_pts_x:
-	# 		q_points.append((p, q_pts_y[q_pts_x.index(p)]))
-	# 	return q_points
-	#
-	# def generate_arc_controls(self, dest, origin, quadrant):
-	# 	m = midpoint(dest, origin)
-	# 	rotation = int(angle_between(dest, origin))
-	# 	angle_1 =  int(np.random.normal(90, 10)) + rotation
-	# 	angle_2 =  int(np.random.normal(90, 10)) + rotation
-	# 	quad_offset = 0
-	# 	if quadrant == 0:
-	# 		quad_offset = 180
-	# 	if quadrant == 1:
-	# 		quad_offset = 90
-	# 	if quadrant == 3:
-	# 		quad_offset += 270
-	# 	angle_1 += quad_offset
-	# 	angle_2 += quad_offset
-	# 	mp_len = int(line_segment_len(dest, m))
-	# 	amplitude_1 = from_range(mp_len // 2, mp_len)
-	# 	amplitude_2 = from_range(mp_len // 2, mp_len)
-	# 	c1 = point_pos(dest, amplitude_1, angle_1)
-	# 	c2 = point_pos(origin, amplitude_2, angle_2)
-	# 	if any(i for i in c1 + c2) < 0:
-	# 		return self.generate_arc_controls(dest, origin, quadrant)
-	# 	d_dest_c1 = line_segment_len(dest, c1)
-	# 	d_dest_c2 = line_segment_len(dest, c2)
-	# 	return [c1, c2] if d_dest_c1 > d_dest_c2 else [c2, c1]
-	#
-	# def generate_figure(self):
-	# 	BOT_L = 0
-	# 	TOP_L = 1
-	# 	TOP_R = 2
-	#
-	# 	initial = (400, from_range(450, 750))
-	# 	segments = []
-	# 	min_segments = 8
-	# 	max_segments = 15
-	# 	min_segments_per_q = 1
-	# 	max_segments_per_q = 4
-	# 	segment_count = from_range(min_segments, max_segments)
-	# 	q_intersects = [(from_range(50, 350), 400), (400, from_range(50, 350)), (from_range(450, 750), 400), initial]
-	# 	avg_dist = 150
-	# 	dist_variance = 50
-	# 	seg_count = from_range(min_segments_per_q, max_segments_per_q)
-	# 	segment_count -=  seg_count
-	# 	if segment_count < 0:
-	# 		seg_count += segment_count
-	# 	if seg_count < 1:
-	# 	for quad in range(0,4):
-	#
-	# 		x_offset = 0 if quad in [BOT_L, TOP_L] else 400
-	# 		y_offset = 0 if quad in [TOP_L, TOP_R] else 400
-	# 		origin = None
-	# 		for j in range(0, seg_count):
-	# 			# generate start and end points for each segment in the quadrant
-	# 			q_points = self.generate_segment_positions(seg_count, x_offset, y_offset, avg_dist, dist_variance)
-	#
-	# 			# set origin to the destination of previous segment
-	# 			o = initial if origin is None else origin
-	#
-	# 			# assign destination point; quadrant intersect for last segment of each quadrant
-	# 			d = q_points[j] if j < seg_count - 1 else q_intersects[quad]
-	#
-	# 			# choose a segment type
-	# 			s = random.choice([KLD_ARC, KLD_LINE])
-	#
-	# 			if s == KLD_LINE:
-	# 				seg_pts = linear_interpolation(o, d, segmented=False)
-	# 			if s == KLD_ARC:
-	# 				m = midpoint(o, d)
-	# 				a = angle_between(o, d)
-	# 				c = point_pos(m, random.choice(range(100, 200)), a + 90)
-	# 				seg_pts = bezier_interpolation(o, d, c, segmented=False)
-	# 			segments += seg_pts
-	# 			# for i in range(len(seg_pts)):
-	# 			# 	segments.append((seg_pts[i][0] + x_offset, seg_pts[i][1] + y_offset))
-	#
-	# 			origin = d
-	#
-	# 	return segments
+		last_f = time.time()
+		f = 0
+		frame_start = time.time()
+		for f in a_frames:
+			while time.time() < rate + last_f:
+				pump()
+			# f += 1
+			# pos = a_frames[f]
+			self.exp.fill()
+			self.exp.blit(surface, 5, Params.screen_c)
+			self.exp.blit(self.exp.tracker_dot, 5, f)
+			self.exp.flip()
+			last_f = time.time()
+		print time.time() - frame_start
