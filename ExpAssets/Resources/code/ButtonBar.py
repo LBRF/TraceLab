07ba2b500ffe2dae6b9cb6f33714ec7fdc216cc3
Austin Hurst <mynameisaustinhurst@gmail.com>
2026-02-09 16:10:30 -0400

    ButtonBar Cleanup (#9)
    * Remove old unused Slider class
    
    * Clean up ButtonBar API and usage
    
    * Clean up Button class
    
    * Further simplify ButtonBar API
    
    * Allow blitting ButtonBar without rendering

--- ExpAssets/Resources/code/ButtonBar.py
+++ ExpAssets/Resources/code/ButtonBar.py
@@ -2,176 +2,29 @@
 __author__ = 'jono'
 
 import time
-import sdl2
 
 import klibs.KLParams as P
-from klibs.KLConstants import RECT_BOUNDARY, CIRCLE_BOUNDARY
 from klibs.KLGraphics import blit, fill, flip
 from klibs.KLGraphics.KLDraw import Rectangle, Ellipse
 from klibs.KLCommunication import message
-from klibs.KLBoundary import BoundaryInspector
-from klibs.KLUtilities import pump, flush, show_mouse_cursor, hide_mouse_cursor, mouse_pos
-from klibs.KLUserInterface import ui_request
+from klibs.KLBoundary import BoundaryInspector, RectangleBoundary
+from klibs.KLEventQueue import pump, flush
+from klibs.KLUserInterface import ui_request, mouse_clicked, show_cursor, hide_cursor
 from klibs.KLEnvironment import EnvAgent
 
 
-class Slider(BoundaryInspector, EnvAgent):
-	def __init__(self, y_pos, bar_length, bar_height, handle_radius, bar_fill, handle_fill):
-		BoundaryInspector.__init__(self)
-		EnvAgent.__init__(self)
-		self.boundaries = {}
-		self.pos = (P.screen_c[0] - bar_length // 2, y_pos)  # upper-left
-		self.message_pos = (P.screen_c[0], y_pos - 50)
-		self.__handle_pos = (self.pos[0], self.pos[1] + bar_height // 2)
-		self.handle_color = handle_fill
-		self.handle_radius = handle_radius
-		self.handle_stroke = None
-		self.handle_boundary = None
-		self.bar = None
-		self.bar_size = (bar_length, bar_height)
-		self.bar_color = bar_fill
-		self.bar_stroke = None
-		self.show_increment_ticks = True
-		self.show_increment_text = False
-		self.increment_count = None
-		self.increments = []
-		self.increment_by = 1
-		self.increment_surfs = {}
-		self.lower_bound = None
-		self.upper_bound = None
-		self.handle = Ellipse(self.handle_radius * 2, fill=self.handle_color, stroke=self.handle_stroke)
-		self.bar = Rectangle(self.bar_size[0], self.bar_size[1], fill=self.bar_color, stroke=self.bar_stroke)
-		self.add_boundary("handle", 
-			[(self.pos[0] + self.handle_radius, self.pos[1]), self.handle_radius],
-			CIRCLE_BOUNDARY
-		)
-		self.msg = message("How many corners did the dot traverse?", "default", blit_txt=False)
-		self.lb_msg = None
-		self.ub_msg = None
-		self.ok_text = message("OK", blit_txt=False)
-		self.ok_inactive_button = Rectangle(100, 50, stroke=(1, (255,255,255)), fill=(125,125,125)).render()
-		self.ok_active_button = Rectangle(100, 50, stroke=(1, (255,255,255)), fill=(5,175,45)).render()
-		self.button_active = False
-		self.button_pos = (P.screen_c[0], y_pos + bar_height + 50)
-		button_upper_left = (self.button_pos[0] - 50, self.button_pos[1] - 25)
-		button_botton_right = (self.button_pos[0] + 50, self.button_pos[1] + 25)
-		self.add_boundary("button", (button_upper_left, button_botton_right), RECT_BOUNDARY)
+class Button(object):
 
-		self.response = None
-
-	def __build_increments(self):
-		self.increment_count = (self.upper_bound + 1 - self.lower_bound) // self.increment_by
-		inc_width = self.bar_size[0] // self.increment_count + 1  # w/o +1 final increment has a px width of 0
-		for i in range(0, self.increment_count):
-			i_range = [self.pos[0] + i * inc_width, self.pos[0] + inc_width + i * inc_width]
-			value = self.lower_bound + i * self.increment_by
-			self.increments.append([i_range, value])
-		for i in range(self.lower_bound, self.upper_bound + 1):
-			self.increment_surfs[i] = (message(str(i), "small", blit_txt=False))
-
-	def __update_handle_boundary(self):
-		if not self.handle_pos:
-			self.handle_pos = self.pos
-		self.boundaries['handle'].bounds = [self.handle_pos, self.handle_radius]
-
-	def update_range(self, seg_count):
-		# this originally did some randomization around a given number and was later changed to hard values
-		self.lower_bound = 1
-		self.upper_bound = 5
-		self.lb_msg = message(str(self.lower_bound), "small", blit_txt=False)
-		self.ub_msg = message(str(self.upper_bound), "small", blit_txt=False)
-		self.__build_increments()
-
-	def slide(self):
-		show_mouse_cursor()
-		self.blit()
-
-		dragging = False
-		while True:
-			if not dragging:
-				m_pos = mouse_pos()
-				for event in pump(True):
-					if event.type == sdl2.SDL_KEYDOWN:
-						ui_request(event.key.keysym)
-					elif event.type in (sdl2.SDL_MOUSEBUTTONDOWN, sdl2.SDL_MOUSEBUTTONUP):
-						within_button = self.within_boundary("button", m_pos)
-						if self.button_active and within_button:
-							return self.response
-				dragging = self.within_boundary("handle", m_pos)
-			if dragging:
-				button_up = False
-				off_handle = False
-				for event in pump(True):
-					if event.type == sdl2.SDL_KEYDOWN:
-						ui_request(event.key.keysym)
-					elif event.type == sdl2.SDL_MOUSEBUTTONUP:
-						button_up = True
-
-				off_handle = not self.within_boundary("handle", mouse_pos())
-
-				if off_handle or button_up:
-					dragging = False
-					self.response =  self.handle_value()
-					self.button_active = True
-					flush()
-					return -1
-				self.handle_pos = mouse_pos()[0]
-				self.blit()
-		return False
-
-	def blit(self):
-		fill()
-		blit(self.ok_active_button if self.button_active else self.ok_inactive_button, 5, self.button_pos, flip_x=P.flip_x)
-		blit(self.ok_text, 5, self.button_pos, flip_x=P.flip_x)
-		blit(self.bar, 7, self.pos, flip_x=P.flip_x)
-		blit(self.handle, 5, self.handle_pos, flip_x=P.flip_x)
-		blit(self.increment_surfs[self.handle_value()], registration=5, location=self.message_pos, flip_x=P.flip_x)
-		blit(self.lb_msg, 5, (self.pos[0], self.pos[1] - 15), flip_x=P.flip_x)
-		blit(self.ub_msg, 5, (self.pos[0] + self.bar_size[0], self.pos[1] - 15), flip_x=P.flip_x)
-		blit(self.msg, 5, P.screen_c, flip_x=P.flip_x)
-		flip()
-
-	def handle_value(self):
-		for i in self.increments:
-			if self.handle_pos[0] in range(i[0][0], i[0][1]):
-				return i[1]
-
-	def reset(self):
-		self.response = None
-		self.button_active = False
-		self.handle_pos = self.pos[0]
-
-	@property
-	def handle_pos(self):
-		return self.__handle_pos
-
-	@handle_pos.setter
-	def handle_pos(self, pos):
-		if pos < self.pos[0]:
-			pos = self.pos[0]
-		if pos > self.pos[0] + self.bar_size[0]:
-			pos = self.pos[0] + self.bar_size[0] - 2
-		self.__handle_pos = (pos, self.__handle_pos[1])
-		self.__update_handle_boundary()
-
-
-class Button(EnvAgent):
-
-	def __init__(self, bar, button_text, button_size, location, callback=None):
-		super(Button, self).__init__()
-		super(EnvAgent, self).__init__()
-		self.bar = bar
-		self.size = button_size
-		self.button_text = button_text
-		self.button_rtext_a = message(button_text, "button_active", blit_txt=False)
-		self.button_rtext_i = message(button_text, "button_inactive", blit_txt=False)
-		self.frame_i = Rectangle(button_size[0], button_size[1], fill=None, stroke=(5, (255,255,255)))
-		self.frame_a = Rectangle(button_size[0], button_size[1], fill=None, stroke=(5, (150,255,150)))
-		self.active = False
+	def __init__(self, label, size, location):
+		self.label = str(label)
+		self.size = size
 		self.location = location
-		self.text_location = (self.location[0] + self.size[0] // 2, self.location[1] + self.size[1] // 2)
-		self.create_boundary()
-		self.callback = callback
+		self.button_rtext_a = message(self.label, "button_active", blit_txt=False)
+		self.button_rtext_i = message(self.label, "button_inactive", blit_txt=False)
+		self.frame_i = Rectangle(size[0], size[1], fill=None, stroke=(5, (255,255,255)))
+		self.frame_a = Rectangle(size[0], size[1], fill=None, stroke=(5, (150,255,150)))
+		self.bounds = self._create_boundary(size, location)
+		self.active = False
 
 	def blit(self):
 		if self.active:
@@ -181,19 +34,16 @@ class Button(EnvAgent):
 			blit(self.frame_i, 5, self.location)
 			blit(self.button_rtext_i, 5, self.location)
 
-	def create_boundary(self):
-		x1 = self.location[0] - self.size[0] // 2
-		y1 = self.location[1] - self.size[1] // 2
-		x2 = self.location[0] + self.size[0] // 2
-		y2 = self.location[1] + self.size[1] // 2
-		self.bar.add_boundary(self.button_text, ((x1,y1), (x2,y2)), RECT_BOUNDARY)
+	def _create_boundary(self, size, loc):
+		xy1 = (loc[0] - size[0] // 2, loc[1] - size[1] // 2)
+		xy2 = (loc[0] + size[0] // 2, loc[1] + size[1] // 2)
+		return RectangleBoundary(self.label, xy1, xy2)
 
 
-class ButtonBar(BoundaryInspector, EnvAgent):
+class ButtonBar(EnvAgent):
 
 	def __init__(self, buttons, button_size, screen_margins, y_offset, message_txt=None, finish_button=True):
 		super(ButtonBar, self).__init__()
-		super(EnvAgent, self).__init__()
 		self.txtm.add_style('button_inactive', 24, [255, 255, 255, 255])
 		self.txtm.add_style('button_active', 24, [150, 255, 150, 255])
 		self.b_count = len(buttons)
@@ -207,7 +57,6 @@ class ButtonBar(BoundaryInspector, EnvAgent):
 			self.b_height = button_size
 		self.screen_margins = screen_margins
 		self.y_offset = y_offset
-		self.b_pad = (P.screen_x - (self.b_width * self.b_count + 2 * self.screen_margins)) // (self.b_count - 1)
 		self.gen_finish_button = finish_button
 		self.finish_b = None
 		self.message_txt = message_txt
@@ -215,96 +64,104 @@ class ButtonBar(BoundaryInspector, EnvAgent):
 			self.message_r = message(message, "instructions", blit_txt=False)
 			self.message_loc = (P.screen_c[0], self.y_offset - (self.message_r.height * 2))
 		self.gen_buttons()
-		self.start = None
-		self.mt = None
-		self.rt = None
-		self.response = None
+		self._loop_start = None
+
+	def _timestamp(self):
+		# Round to nearest 0.1 millisecond for more readable values
+		return round(time.perf_counter(), 4)
 
 	def gen_buttons(self):
+		w, h = (self.b_width, self.b_height)
+		margins = self.screen_margins
+		pad = (P.screen_x - (w * self.b_count + 2 * margins)) // (self.b_count - 1)
 		for b in self.button_data:
 			i = self.button_data.index(b)
-			loc = (self.screen_margins + (i * self.b_width) + (i * self.b_pad) + self.b_width // 2, \
-				   self.y_offset + self.b_height // 2)
-			try:
-				self.buttons.append(Button(self, str(b[0]), (self.b_width, self.b_height), loc, b[2]))
-			except IndexError:
-				self.buttons.append(Button(self, str(b[0]), (self.b_width,self.b_height), loc))
+			loc = (margins + (i * w) + (i * pad) + w // 2, self.y_offset + h // 2)
+			self.buttons.append(Button(b, (w, h), loc))
 		if self.gen_finish_button:
-			self.finish_b = Button(self, "Done", (100,50), \
-								   (P.screen_x - (self.screen_margins + self.b_width), int(P.screen_y * 0.9)))
+			self.finish_b = Button(
+				"Done", (100, 50), (P.screen_x - (margins + w), int(P.screen_y * 0.9))
+			)
+
+	def blit(self):
+		for b in self.buttons:
+			b.blit()
+		if self.finish_b:
+			self.finish_b.blit()
 
 	def render(self):
 		fill()
-		for b in self.buttons:
-			b.blit()
-		try:
-			self.finish_b.blit()
-		except AttributeError:
-			pass
+		self.blit()
 		if self.message_txt:
 			blit(self.message_r, 5, self.message_loc)
 		flip()
 
-	def collect_response(self):
-		self.start = time.time()
-		finished = False
-		selection = None
-		last_selected = None
-		flush()
-		mt_start = None
-		while not finished:
-			show_mouse_cursor()
-			events = pump(True)
-			for e in events:
-				if e.type == sdl2.SDL_KEYDOWN:
-					ui_request(e.key.keysym)
-				elif e.type == sdl2.SDL_MOUSEBUTTONDOWN:
-					selection = None
-					for b in self.buttons:
-						if self.within_boundary(b.button_text, [e.button.x, e.button.y]):
-							self.toggle(b)
-							if not self.rt:
-								self.rt = time.time() - self.start
-								mt_start = time.time()
-							if b.active:
-								selection = b
-								last_selected = b
-								if callable(b.callback):
-									if self.finish_b is None:
-										return b.callback
-									else:
-										b.callback()
-						try:
-							if self.finish_b.active and self.within_boundary("Done",[e.button.x, e.button.y]):
-								self.response = int(last_selected.button_text)
-								self.mt = time.time() - mt_start
-								finished = True
-						except AttributeError:
-							pass
-			try:
-				self.finish_b.active = selection is not None
-			except AttributeError:
-				pass
+	def collect(self):
+		"""Renders and collects a response from the button bar.
+
+		Response format is `(button, elapsed)` if the button bar has no response
+		button, or `(button, rt_first, rt_final, elapsed)`:
+		  * 'button' is the label of the response button.
+		  * 'elapsed' is the duration (in seconds) between the start and end of
+		     the collection loop.
+		  * 'rt_first' is the reaction time (in seconds) for the participant's
+		     first button selection.
+		  * 'rt_final' is the reaction time (in seconds) for the participant's
+		     final buttion selection. Only different from 'rt_first' if the
+			 participant changes their response before submitting.
+
+		"""
+		rt_first = None
+		rt_final = None
+		choice = None
+		resp = None
+		self.render()
+		self.init()
+		while not resp:
+			events = pump()
+			# If no finish button provided, return as soon as a button is clicked
+			if self.finish_b is None:
+				for b in self.buttons:
+					if mouse_clicked(within=b.bounds, queue=events):
+						choice = b.label
+						elapsed = self._timestamp() - self._loop_start
+						resp = (choice, elapsed)
+						break
+			else:
+				# Check if any button has been clicked, changing its state if so
+				for b in self.buttons:
+					if mouse_clicked(within=b.bounds, queue=events):
+						self.toggle(b)
+						choice = b.label if b.active else None
+						self.finish_b.active = True if b.active else False
+						if not rt_first:
+							rt_first = (self._timestamp() - self._loop_start)
+						rt_final = (self._timestamp() - self._loop_start)
+				# If a response has been made and finish button clicked, return response
+				if self.finish_b.active:
+					if mouse_clicked(within=self.finish_b.bounds, queue=events):
+						elapsed = self._timestamp() - self._loop_start
+						resp = (choice, rt_first, rt_final, elapsed)
+						break
 			self.render()
-		fill()
-		flip()
-		hide_mouse_cursor()
+		self.cleanup()
+		return resp
+
+	def init(self):
+		flush()
+		show_cursor()
+		self._loop_start = self._timestamp()
+
+	def cleanup(self):
+		hide_cursor()
+		for b in self.buttons:
+			b.active = False
+		if self.finish_b:
+			self.finish_b.active = False
 
 	def toggle(self, button):
 		for b in self.buttons:
 			b.active = not b.active if b == button else False
-
-	def reset(self):
-		self.start = None
-		self.rt = None
-		self.mt = None
-		self.response = None
-		for b in self.buttons:
-			b.active = False
-		try:
-			self.finish_b.active = False
-		except AttributeError:
-			pass
 
 	def update_message(self, message_text):
 		self.message_txt = message_text
--- experiment.py
+++ experiment.py
@@ -105,7 +105,6 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 	practice_buttons = None
 	practice_instructions = None
 	practice_button_bar = None
-	practice_kf = None
 
 
 	def __init__(self):
@@ -119,6 +118,8 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 
 		self.figure_name = P.practice_figure
 		self.animate_time = P.practice_animation_time
+
+		self.__practicing__ = True
 		self.setup_response_collector()
 		self.trial_prep()
 		self.evm.start()
@@ -128,6 +129,7 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 			pass
 		self.evm.reset()
 		self.trial_clean_up()
+		self.__practicing__ = False
 
 
 	def setup(self):
@@ -184,9 +186,8 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 		# Initialize messages and response buttons for control trials
 		control_fail_txt = "Please keep your finger on the start area for the complete duration."
 		self.control_fail_msg = message(control_fail_txt, 'error', blit_txt=False)
-		ctrl_buttons = ["1", "2", "3", "4", "5"]
 		self.control_bar = ButtonBar(
-			buttons = [(i, P.btn_size, None) for i in ctrl_buttons],
+			buttons = ["1", "2", "3", "4", "5"],
 			button_size = P.btn_size, screen_margins = P.btn_s_pad, y_offset = P.y_pad,
 			message_txt = P.control_q
 		)
@@ -210,13 +211,8 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 			P.practice_instructions, "instructions",
 			align="center", blit_txt=False
 		)
-		practice_buttons = [
-			('Replay', [200, 100], self.practice),
-			('Practice', [200, 100], self.__practice__),
-			('Begin', [200, 100], any_key)
-		]
 		self.practice_button_bar = ButtonBar(
-			practice_buttons,
+			["Replay", "Practice", "Begin"],
 			[200, 100], P.btn_s_pad, P.y_pad, finish_button=False
 		)
 
@@ -254,7 +250,7 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 
 			if P.enable_practice:
 				# Load tutorial animation for current condition, play it, and enter practice
-				self.practice_kf = FrameSet(new_instructions['frames'], "assets")
+				self.tutorial = FrameSet(new_instructions['frames'], "assets")
 				if P.block_number == 1:
 					fill()
 					blit(self.practice_instructions, 5, P.screen_c)
@@ -262,7 +258,8 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 					any_key()
 				else:
 					self.start_trial_button()
-				self.practice()
+				self.tutorial.play()
+				self.practice_menu()
 
 			self.prev_response_type = self.response_type
 
@@ -392,7 +389,6 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 			outpath = os.path.join(self.fig_dir, self.file_name + ".zip")
 			save_figure(outpath, self.figure, self.drawing)
 		self.rc.draw_listener.reset()
-		self.control_bar.reset()
 
 
 	def clean_up(self):
@@ -533,16 +529,14 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 
 	def control_trial(self):
 
-		if P.dm_always_show_cursor:
-			show_mouse_cursor()
+		control_q_txt = P.control_q.format(self.control_question)
+		self.control_bar.update_message(control_q_txt)
 
-		self.control_bar.update_message(P.control_q.format(self.control_question))
-		self.control_bar.render()
-		self.control_bar.collect_response()
+		resp, rt_first, rt_final, mt = self.control_bar.collect()
 
-		self.rt = self.control_bar.rt
-		self.mt = self.control_bar.mt
-		self.control_response = self.control_bar.response
+		self.rt = rt_final
+		self.mt = mt
+		self.control_response = resp
 
 
 	def _generate_figure(self, duration):
@@ -660,31 +654,15 @@ class TraceLab(klibs.Experiment, BoundaryInspector):
 		self.evm.reset()
 
 
-	def practice(self, play_key_frames=True, callback=None):
+	def practice_menu(self):
 
-		self.__practicing__ = True
-
-		if callback == self.__practice__:
-			play_key_frames = False
-			self.__practice__()
-		elif callback == self.practice:
-			play_key_frames = True
-		elif callback == any_key:
-			self.__practicing__ = False
-			return any_key()
-
-		if play_key_frames:
-			self.practice_kf.play()
-
-		self.practice_button_bar.reset()
-		self.practice_button_bar.render()
-		self.evm.start()
-		cb = self.practice_button_bar.collect_response()
-		self.evm.reset()
-
-		self.__practicing__ = False
-
-		return self.practice(callback=cb)
+		choice = None
+		while choice != "Begin":
+			choice, rt = self.practice_button_bar.collect()
+			if choice == "Replay":
+				self.tutorial.play()
+			elif choice == "Practice":
+				self.__practice__()
 
 
 	def log(self, msg):
